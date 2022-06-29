57d56a93fed0f46ca2ad92c0f979185c10416d4c
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2022-06-29 17:33:02 +0200

    added test file.

---
+++ src/test.cpp
@@ -0,0 +1,25 @@
+
+#include "GenArray.h"
+
+
+int main() {
+
+  /** testing array */
+  GenArray<int, 5> testArray;
+  unsigned int element1[5] = {1,1,2,3,4};
+  unsigned int element2[5] = {0,1,4,3,4};
+  unsigned int element3[5] = {2,2,2,3,4};
+  unsigned int element4[5] = {2,2,2,3,5};
+  testArray.SetEntry(element1, 5);
+  testArray.SetEntry(element2, 6);
+  testArray.SetEntry(element3, 7);
+  std::cout<<" entry "<<testArray.GetEntry(element1)<<std::endl;
+  std::cout<<" entry "<<testArray.GetEntry(element2)<<std::endl;
+  std::cout<<" entry "<<testArray.GetEntry(element3)<<std::endl;
+  std::cout<<" entry "<<testArray.GetEntry(element4)<<std::endl;
+
+  std::cout<<" Dim "<<testArray.GetDimension()<<std::endl;
+  std::cout<<" Size "<<testArray.GetSize()<<std::endl;
+
+  return 0;
+}
